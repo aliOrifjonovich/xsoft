3f74d3a68830d138643a848898fea88a3ffbe4c0
aliOrifjonovich <orifjonovichalisher@gmail.com>
2025-04-02 16:08:46 +0500

    changed phone input as component

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -7,8 +7,9 @@ import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Eye, EyeOff } from "lucide-react";
+import { Eye, EyeOff, Loader2 } from "lucide-react";
 import { toast } from "sonner";
+import { PhoneInput } from "./ui/phone-input";
 
 interface LoginFormInputs {
   phonenumber: string;
@@ -20,19 +21,23 @@ export function LoginForm({
   ...props
 }: React.ComponentProps<"form">) {
   const [showPassword, setShowPassword] = useState(false);
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [loading, setLoading] = useState(false);
   const { register, handleSubmit } = useForm<LoginFormInputs>();
   const router = useRouter();
 
   // Function to handle login
   const onSubmit = async (data: LoginFormInputs) => {
     try {
+      setLoading(true);
+      // Make a POST request to the login API
       const response = await fetch(
         "https://carmanagement-1-rmyc.onrender.com/api/v1/token/",
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
-            identifier: data.phonenumber,
+            identifier: phoneNumber,
             password: data.password,
           }),
         }
@@ -44,8 +49,6 @@ export function LoginForm({
 
       const result = await response.json();
       Cookies.set("token", result.access, { secure: true });
-      router.push("/");
-
       toast.success("Login muvaffaqiyatli amalga oshirildi", {
         position: "top-right",
         closeButton: true,
@@ -55,8 +58,17 @@ export function LoginForm({
         },
       });
     } catch (error) {
-      console.error("Login failed:", error);
-      toast.error("Login failed. Please try again.");
+      toast.error("Login failed. Please try again.", {
+        position: "top-right",
+        closeButton: true,
+        style: {
+          backgroundColor: "red",
+          color: "white",
+        },
+      });
+    } finally {
+      router.push("/");
+      setLoading(false);
     }
   };
 
@@ -76,12 +88,17 @@ export function LoginForm({
         {/* Phone Number Field */}
         <div className="grid gap-3">
           <Label htmlFor="phonenumber">Telefon raqam</Label>
-          <Input
+          <PhoneInput
+            onChange={setPhoneNumber}
+            value={phoneNumber}
+            defaultCountry="UZ"
+          />
+          {/* <Input
             id="phonenumber"
             type="text"
             placeholder="+99890 123 45 67"
             {...register("phonenumber", { required: true })}
-          />
+          /> */}
         </div>
 
         {/* Password Field */}
@@ -113,7 +130,13 @@ export function LoginForm({
 
         {/* Submit Button */}
         <Button type="submit" className="w-full">
-          Login
+          {loading ? (
+            <span className="loader">
+              <Loader2 className="animate-spin h-4 w-4" />
+            </span>
+          ) : (
+            "Login"
+          )}
         </Button>
       </div>
     </form>
